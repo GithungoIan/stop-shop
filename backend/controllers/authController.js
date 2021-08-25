c4db4a5b722d57e5febab9bda5a35c2fc4336353
GithungoIan <githungoian53@gmail.com>
2021-08-25 14:39:14 +0300

    feat: user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/userModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const Email = require("../utils/email");
+const jwt = require("jsonwebtoken");
 
 // signup
 exports.signup = catchAsync(async (req, res, next) => {
@@ -24,7 +25,53 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 // login
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password exists
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+  // check if user exist and password is correct
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  // if everything is correct send the token to the client
+  createSendToken(user, 200, req, res);
+});
 // log out
 // forgot password
 // reset password
