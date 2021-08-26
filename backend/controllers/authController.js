b6e02922900c9e941578c8898727ad24f6f13939
GithungoIan <githungoian53@gmail.com>
2021-08-26 06:52:41 +0300

    feat: protect user routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,7 @@ const catchAsync = require("../utils/catchAsync");
 const Email = require("../utils/email");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const { promisify } = require("util");
 
 // signup
 exports.signup = catchAsync(async (req, res, next) => {
@@ -170,5 +171,47 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 });
 
 // protect
+exports.protect = catchAsync(async (req, res, next) => {
+  // getting token ad check if it's There
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to gain access", 401)
+    );
+  }
+
+  // verify token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // check if user stil exist
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError("The user belonging to this token does not exist", 401)
+    );
+  }
+
+  // check if the user changed psswords after
+  if (currentUser.changePasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed passwords! Please log in again", 401)
+    );
+  }
+
+  // grant access to the protected route
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
 // restrict to
 // is logged in
