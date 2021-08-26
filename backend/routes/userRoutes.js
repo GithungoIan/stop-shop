15164ff8d6b7a02ab521af3602f29dfdc24d2fb6
GithungoIan <githungoian53@gmail.com>
2021-08-26 07:09:12 +0300

    updated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+
 const router = express.Router();
+
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+
+// protect all routes
+router.use(authController.protect);
+
+router.patch("/updateMyPassword", authController.updatePassword);
+
+// restrict only to admin
+router.use(authController.restrictTo("admin"));
 
 router.route("/").get(userController.getAllUsers);
 router
